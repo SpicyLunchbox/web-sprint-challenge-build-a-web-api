c01d8fe28deac52b07d15632e2cf3f5f05fcf426
Weston Woodard <westonwoodard28@gmail.com>
2021-04-09 11:38:21 -0700

    actions router complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -6,40 +6,65 @@ const router = express.Router(); // creates router using express
 
 // [GET] /api/actions returns an array of actions (or an empty array) as the body of the response.
 router.get(`/`, (req,res) => {
-
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            res.status(500).json({message: `unable to retrieve actions`})
+        })
 })
-
 
 
 
 // [GET] /api/actions/:id returns an action with the given id as the body of the response.
 router.get(`/:id`, (req,res) => {
-
+    router.get(req.params.id)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({message: `unable to retrieve action`})
+        })
 })
 
 
 
 // [POST] /api/actions returns the newly created action as the body of the response.
 router.post(`/`, (req,res) => {
-
+    router.insert(req.body)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({message: `unable to create new action`})
+        })
 })
-
 
 
 
 // [PUT] /api/actions/:id returns the updated action as the body of the response.
 router.put(`/:id`, (req,res) => {
-
+    const id = req.params.id
+    const changes = req.body
+    router.update(id,changes)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({message: `unable to update action`})
+        })
 })
-
-
-
 
 
 
 // [DELETE] /api/actions/:id returns no response body.
 router.delete(`/:id`, (req,res) => {
-
+    router.remove(req.params.id)
+        .then()
+        .catch(err => {
+            res.status(500).json({message: `unable to delete action`})
+        })
 })
 
 
